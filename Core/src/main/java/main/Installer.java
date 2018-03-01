52825f14f794ec7fb69de74c4b923ce466c47df4
Xrosby <mads.sfg@gmail.com>
2018-03-01 19:03:35 +0100

    libgdx dependencies in core pom

---
+++ Core/src/main/java/main/Installer.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.openide.modules.ModuleInstall;
+
+public class Installer extends ModuleInstall {
+
+    @Override
+    public void restored() {
+       LwjglApplicationConfiguration cfg =
+			new LwjglApplicationConfiguration();
+		cfg.title = "Asteroids";
+		cfg.width = 1400;
+		cfg.height = 900;
+		cfg.useGL30 = false;
+		cfg.resizable = false;
+		
+		new LwjglApplication(new Game(), cfg);
+		
+    }
+
+}
