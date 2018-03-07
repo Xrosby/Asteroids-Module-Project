3ca16cdcf49ff7de0e13fe4a68569a7b196d7a04
Xrosby <mads.sfg@gmail.com>
2018-03-07 08:48:17 +0100

    Asteroids netbeans module version

--- Core/src/main/java/main/Installer.java
+++ Core/src/main/java/main/Installer.java
@@ -7,22 +7,33 @@ package main;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.net.MalformedURLException;
+import java.net.URL;
 import org.openide.modules.ModuleInstall;
+import org.openide.util.Exceptions;
 
 public class Installer extends ModuleInstall {
 
     @Override
     public void restored() {
-       LwjglApplicationConfiguration cfg =
-			new LwjglApplicationConfiguration();
-		cfg.title = "Asteroids";
-		cfg.width = 1400;
-		cfg.height = 900;
-		cfg.useGL30 = false;
-		cfg.resizable = false;
-		
-		new LwjglApplication(new Game(), cfg);
-		
+
+   
+        
+        //https://opengameart.org/content/spaceship-set-4-pixel-art-space-ships#
+        
+        
+        LwjglApplicationConfiguration cfg
+                = new LwjglApplicationConfiguration();
+        cfg.title = "Asteroids";
+        cfg.width = 1400;
+        cfg.height = 900;
+        cfg.useGL30 = false;
+        cfg.resizable = false;
+
+        new LwjglApplication(new Game(), cfg);
+
     }
 
 }
